23717aa2f4d37288791bfe9758c35755c21cbbb0
kcalva <kcalva737@gmail.com>
2024-06-11 17:00:47 -0500

    adding additional changes. I have an error iterating through movies that have same director

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,18 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+  def directorpage
+    render({ :template => "misc_templates/directors"})
+  end
+  def directorinfo
+    director_id = params.fetch("director_id")
+    @x = Director.where({ :id => director_id }).at(0)
+    @matching_movies = Movie.where({:director_id => director_id}).at(0)
+
+    render({ :template => "misc_templates/directorInfo"})
+  end
+  def moviepage
+
+    render({ :template => "misc_templates/movies"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "misc", :action => "directorpage" })
+  get("/directors/:director_id", { :controller => "misc", :action => "directorinfo" })
+  get("/movies", { :controller => "misc", :action => "moviepage" })
+  get("/movies/:movie_id", { :controller => "misc", :action => "movieinfo" })
 end
