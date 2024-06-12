d8e66cfa10474fe319bfc8de3e98df4c5b09fff3
kcalva <kcalva737@gmail.com>
2024-06-12 17:00:29 -0500

    adding more details that fixed my errors, still need to add filmography to the actors section

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -7,13 +7,34 @@ class MiscController < ApplicationController
   end
   def directorinfo
     director_id = params.fetch("director_id")
-    @x = Director.where({ :id => director_id }).at(0)
-    @matching_movies = Movie.where({:director_id => director_id}).at(0)
-
+    @director = Director.where({ :id => director_id }).at(0)
+    @director_movies = Movie.where({:director_id => director_id})
     render({ :template => "misc_templates/directorInfo"})
   end
   def moviepage
-
     render({ :template => "misc_templates/movies"})
+  end
+  def movieinfo
+    movie_id = params.fetch("movie_id")
+    @movie = Movie.where({:id => movie_id}).at(0)
+    @movie_director =  Director.where({ :id => @movie.director_id }).at(0)
+
+    render({ :template => "misc_templates/movieInfo"})
+  end
+  def youngestpage
+    @youngest_director = Director.where.not({ :dob => nil }).order({ :dob => :desc }).at(0)
+    render({ :template => "misc_templates/youngest"})
+  end
+  def eldestpage
+    @eldest_director = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+    render({ :template => "misc_templates/eldest"})
+  end
+  def actorpage
+    render({ :template => "misc_templates/actor"})
+  end
+  def actorinfo
+    actor_id = params.fetch("actor_id")
+    @actor = Actor.where({:id => actor_id}).at(0)
+    render({ :template => "misc_templates/actorInfo"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "misc", :action => "directorpage" })
+  get("/directors/youngest", { :controller => "misc", :action => "youngestpage" })
+  get("/directors/eldest", { :controller => "misc", :action => "eldestpage" })
   get("/directors/:director_id", { :controller => "misc", :action => "directorinfo" })
   get("/movies", { :controller => "misc", :action => "moviepage" })
   get("/movies/:movie_id", { :controller => "misc", :action => "movieinfo" })
+  get("/actors", { :controller => "misc", :action => "actorpage" })
+  get("/actors/:actor_id", { :controller => "misc", :action => "actorinfo" })
+
 end
