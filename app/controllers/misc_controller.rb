6ca007e7c29a81d275f4c8d6c9b1bc01c71ef233
kcalva <kcalva737@gmail.com>
2024-06-13 16:20:06 -0500

    understanding how to  test and debug code. I now see we had to iterate through the character table and find the movie id associated with character to get movie info

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,18 +2,22 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
   def directorpage
     render({ :template => "misc_templates/directors"})
   end
+
   def directorinfo
     director_id = params.fetch("director_id")
     @director = Director.where({ :id => director_id }).at(0)
     @director_movies = Movie.where({:director_id => director_id})
     render({ :template => "misc_templates/directorInfo"})
   end
+
   def moviepage
     render({ :template => "misc_templates/movies"})
   end
+
   def movieinfo
     movie_id = params.fetch("movie_id")
     @movie = Movie.where({:id => movie_id}).at(0)
@@ -21,20 +25,26 @@ class MiscController < ApplicationController
 
     render({ :template => "misc_templates/movieInfo"})
   end
+
   def youngestpage
     @youngest_director = Director.where.not({ :dob => nil }).order({ :dob => :desc }).at(0)
     render({ :template => "misc_templates/youngest"})
   end
+
   def eldestpage
     @eldest_director = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
     render({ :template => "misc_templates/eldest"})
   end
+
   def actorpage
     render({ :template => "misc_templates/actor"})
   end
+
   def actorinfo
     actor_id = params.fetch("actor_id")
-    @actor = Actor.where({:id => actor_id}).at(0)
+    @actor = Actor.find_by(id: actor_id)
+    @characters = Character.where(actor_id: actor_id)
+
     render({ :template => "misc_templates/actorInfo"})
   end
 end
